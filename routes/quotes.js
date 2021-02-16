2129944229e1fa802a18cc34e9c531a6ed782e28
Ferdi Ramdhon <ferdirn@gmail.com>
2021-02-16 23:07:05 +0700

    add routes/quotes to app

--- routes/quotes.js
+++ routes/quotes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET quotes listing. */
 router.get('/', function(req, res, next) {
-    res.jsonn({
+    res.json({
         data: [
             {
                 quote: 'First, solve the problem. Then, write the code.',
